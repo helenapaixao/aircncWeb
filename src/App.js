dad3cf455eda3595e9505d1d6ffc105a8a829204
Helena Paixão <hp.helenapaixao@gmail.com>
2019-10-12 23:18:56 -0300

    config app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <h2>Hello</h2>
+  return (
+    <div className="Container">
+
+    </div>
+    )
 }
 
 export default App;
