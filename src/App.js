5136011fb5f2ba5e616dadbb73d0cbad309fdc33
Helena Paixão <hp.helenapaixao@gmail.com>
2019-10-12 23:25:58 -0300

    config css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
-
+import logo from './assets/logo.svg'
 function App() {
   return (
-    <div className="Container">
-
+    <div className="container">
+      <img src={logo} alt='AirCnC'/>
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+        </p>
+        <form>
+          <label htmlFor="email">E-mail *</label>
+          <input 
+          type="email"
+           id="email" 
+           placeholder="Seu melhor e-mail"
+           />
+        </form>
+        <button type="submit">Entrar</button>
+      </div>
     </div>
     )
 }
